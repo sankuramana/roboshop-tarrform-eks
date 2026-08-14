3c6d8d974f291fc3b79092bf5b078c328f74b6ae
ramana sanku <sankuramana456@gmail.com>
2026-08-14 18:24:35 +0530

    feature-barnch

---
+++ 90-eks/variable.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z06065301EXQ0PVL8OM78"
+}
+
+variable "domain_name" {
+    default = "aws45.fun"
+}
+
+variable "eks_version" {
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
+}
